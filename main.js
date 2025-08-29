8b3d2c63612e951732eb29fb3fe6305bf73f1724
johnathancosgrave <28jcosgrave@cambridgecityschools.org>
2025-08-29 09:14:37 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webviewTag: true // ✅ allow <webview>
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
